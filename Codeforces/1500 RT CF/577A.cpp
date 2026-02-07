1fa2b8d43c7ec5937ee44bd4f773b13773d8a94d
Mahfuz Uddin <mahfux090@gmail.com>
2026-02-07 22:37:10 +0600

    Add 1500, Add 1500

---
+++ Codeforces/1500 RT CF/577A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define sz(x) (int)(x).size()
+#define lb '\n'
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9+7;
+
+void solve() {
+    ll n,x;
+    cin>>n>>x;
+    int c=0;
+    for(int i=1;i<=n;i++){
+        if(x%i==0&&x/i<=n)c++;
+    }
+    cout<<c<<lb;
+}
+
+int main() {
+    fast
+    int t=1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
